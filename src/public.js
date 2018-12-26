4c53bb918924ce2695d5945fe4f88da63d22e55d
Dario Rodighiero <fumoseaffabulazioni@gmail.com>
2018-12-26 20:54:11 +0100

    Revert "Moved to http://localhost:3000/"
    This reverts commit b1cb5814ec40d1ca60d9531b755b7dad32e2b081.

--- src/public.js
+++ src/public.js
@@ -1,13 +1,15 @@
 import config from './settings/config'
 import UI from './ui/ui'
 
-require('marx-css/css/marx.css')
+import 'marx-css/css/marx.css'
 
-require('../assets/css/mainSpinner.css')
-require('../assets/css/credits.css')
-require('../assets/css/general.css')
-require('../assets/css/toggle.css')
+import '../assets/css/mainSpinner.css'
+import '../assets/css/credits.css'
+import '../assets/css/general.css'
+import '../assets/css/toggle.css'
 
-const json = require('../assets/data/network-0.2.json')
-
-UI().init(json, true)
+fetch('/api/public/network', { credentials: 'include' }).then(d => d.json())
+	.then(resp => {
+		const data = resp.result		
+		UI().init(data)
+	})
