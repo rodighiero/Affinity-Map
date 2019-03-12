0e77a24ee82651a863631765f81a3a83146e9f38
Dario Rodighiero <fumoseaffabulazioni@gmail.com>
2019-03-12 16:07:31 -0400

    Refactoring Init

--- src/main/init.js
+++ src/main/init.js
@@ -7,37 +7,31 @@ import state from '../settings/state'
 import lab from '../elements/chord'
 import { middleSpace } from '../tools/generalTools'
 
-import a from '../tools/affinities'
+import affinities from '../tools/affinities'
 import { initCaches } from '../elements/cachedElements'
 
 state.debug = new URL(window.location.href).searchParams.get('debug') !== null
 
 export const init = data => {
 
-	const graph = data.graph
-
-	state.init(data)
-
-	a.init(data.description)
-
 	window.state = state
 	window.config = config
 
-	const affs = a.acronyms()
-	const vAffs = a.visibleAcronyms()
+	state.init(data)
+	affinities.init(data.description)
 
-	// create an object like : { aff1:0, aff2:0, ...}
-	const nullAffinitiesObj = affs.reduce((o, v) => ({ ...o, [v]: 0 }), {})
+	// Object { aff1:0, aff2:0, ...}
+	const aObject = affinities.acronyms().reduce((o, v) => ({ ...o, [v]: 0 }), {})
 
 	// Split laboratory names
-	graph.nodes.forEach(lab => {
+	data.graph.nodes.forEach(lab => {
 		const i = middleSpace(lab.attr.enName)
 		lab.attr.enName_a = lab.attr.enName.slice(0, i)
 		lab.attr.enName_b = lab.attr.enName.slice(i + 1)
 	})
 
 	// Split scholar names
-	graph.nodes.forEach(lab => {
+	data.graph.nodes.forEach(lab => {
 		lab.network.nodes.forEach(person => {
 			const i = middleSpace(person.attr.name)
 			person.attr.name_a = person.attr.name.slice(0, i)
@@ -46,7 +40,7 @@ export const init = data => {
 	})
 
 	// Initialize chord diagrams and ribbon
-	state.chordLayouts = graph.nodes.reduce((o, node) => {
+	state.chordLayouts = data.graph.nodes.reduce((o, node) => {
 		// create an empty matrix, if its current length is 0
 		if (node.network.matrix.length === 0) {
 			const internalNodeLength = node.network.nodes.length
@@ -61,13 +55,13 @@ export const init = data => {
 
 	// compute the mean link.sizes values, store them into link.std
 	// Set metrics.values for the links
-	const meanLinkValue = graph.links.reduce((o, link) => {
-		affs.forEach(k => o[k] += link.metrics.values[k] / graph.links.length)
+	const meanLinkValue = data.graph.links.reduce((o, link) => {
+		affinities.acronyms().forEach(k => o[k] += link.metrics.values[k] / data.graph.links.length)
 		return o
-	}, { ...nullAffinitiesObj })
+	}, { ...aObject })
 
-	graph.links.forEach(link => {
-		link.metrics.std = a.acronyms().reduce((o, aff) => {
+	data.graph.links.forEach(link => {
+		link.metrics.std = affinities.acronyms().reduce((o, aff) => {
 			return { ...o, [aff]: link.metrics.values[aff] / meanLinkValue[aff] }
 		}, {})
 	})
@@ -75,15 +69,15 @@ export const init = data => {
 
 	// Set satellites rings width
 
-	const maxLinks = graph.links.reduce((o, link) => {
-		const sum = vAffs.reduce((o, v) => o + link.metrics.values[v], 0)
+	const maxLinks = data.graph.links.reduce((o, link) => {
+		const sum = affinities.visibleAcronyms().reduce((o, v) => o + link.metrics.values[v], 0)
 		return o > sum ? o : sum
 	}, 0)
 
 	const scale = scaleLinear().domain([1, maxLinks]).range([config.satellite.width.min, config.satellite.width.max])
 
-	graph.links.forEach(link => {
-		link.metrics.widths = a.acronyms().reduce((o, aff) => {
+	data.graph.links.forEach(link => {
+		link.metrics.widths = affinities.acronyms().reduce((o, aff) => {
 			o[aff] = scale(link.metrics.values[aff])
 			return o
 		}, {})
@@ -91,11 +85,11 @@ export const init = data => {
 
 	// Set satellite radius
 
-	graph.links.forEach(link => {
+	data.graph.links.forEach(link => {
 		const widths = link.metrics.widths,
 			values = link.metrics.values
 		link.satelliteRadius = config.satellite.radius + config.satellite.width.gap * 4 +
-			vAffs.reduce((o, v) => {
+		affinities.visibleAcronyms().reduce((o, v) => {
 				return o + (values[v] > 0 ? widths[v] : config.satellite.width.empty)
 			}, 0)
 	})
