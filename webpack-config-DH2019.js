843b2194feb8a7921c0d6b5191e1ba4709875fb0
Dario Rodighiero <fumoseaffabulazioni@gmail.com>
2019-12-02 10:29:33 -0500

    GitHub Pages Update

---
+++ webpack-config-DH2019.js
@@ -0,0 +1,62 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  output: {
+    // filename: 'bundle.js',
+    // filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'docs')
+    // chunkFilename: '[name].bundle.js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        type: 'javascript/auto',
+        test: /\.json$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: "[name].[ext]"
+          }
+        }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/html/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
 module.exports = {
+	output: {
+		// filename: 'bundle.js',
+		// filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'docs')
+		// chunkFilename: '[name].bundle.js'
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all'
+		}
+	},
 	module: {
 		rules: [
 			{
