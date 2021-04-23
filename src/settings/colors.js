733380ec9c7a39484bdb064b3992cc703fe82fd6
Dario Rodighiero <2089004+rodighiero@users.noreply.github.com>
2021-04-23 14:04:51 +0200

    Revert "Book"
    This reverts commit 3bcf51b2e12d2d76e872dd470c06c668df8388b2.

--- src/settings/colors.js
+++ src/settings/colors.js
@@ -3,12 +3,10 @@ import state from './state'
 
 // Setting color palettes
 
-const white = rgb(255, 255, 255)
-
 const enac = {
-	IA: white,
-	IIC: white,
-	IIE: white,
+	IA: lab(50, 69, 48),
+	IIC: lab(50, 12, -75),
+	IIE: lab(50, -14, 64),
 }
 
 const elements = {
@@ -17,8 +15,8 @@ const elements = {
 	lighterBackground: rgb(45, 45, 45).toString(),
 	filteredBackground: rgb(45, 45, 45, .8).toString(),
 	placeholder: rgb(80, 80, 80).toString(),
-	link: rgb(255, 255, 255, .5).toString(),
-	orbits: rgb(255, 255, 255, .4).toString(),
+	link: rgb(80, 80, 80).toString(),
+	orbits: rgb(50, 50, 50).toString(),
 	externalNode: rgb(60, 60, 60),
 	keywordsOn: rgb(220, 220, 220).toString(),
 	keywordsOff: rgb(150, 150, 150).toString(),
